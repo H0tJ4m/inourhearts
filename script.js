5c54531a21f94fc976b4e48964e508e058a9d78c
H0tJ4m <156412668+H0tJ4m@users.noreply.github.com>
2024-01-12 15:13:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-window.onload = function() {
-    var speech = new SpeechSynthesisUtterance();
-    speech.text = document.getElementById("loveLetter").innerText;
-    speech.lang = 'en-US';
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = 1;
+document.addEventListener('DOMContentLoaded', function() {
+    var playButton = document.createElement('button');
+    playButton.innerText = 'Read the Letter';
+    playButton.onclick = function() {
+        var speech = new SpeechSynthesisUtterance();
+        speech.text = document.getElementById("loveLetter").innerText;
+        speech.lang = 'en-US';
+        speech.volume = 1;
+        speech.rate = 1;
+        speech.pitch = 1;
 
-    window.speechSynthesis.speak(speech);
-};
+        window.speechSynthesis.speak(speech);
+    };
+
+    document.body.appendChild(playButton);
+});
